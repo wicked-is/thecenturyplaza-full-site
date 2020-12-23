bb44e3a02c8d370e0a0fe60445ac8e617076e77c
Ilya Derets <iderets@gmail.com>
2020-12-23 17:46:52 +0200

    Remove console log

--- src/shared/components/SEO.jsx
+++ src/shared/components/SEO.jsx
@@ -3,7 +3,6 @@ import { Helmet } from "react-helmet";
 
 const SEO = ({ '*': path, seoData }) => {
   const { title, description } = seoData.routes[path] || seoData.default;
-  console.log({ path, title, description })
   return (
     <Helmet>
       <title>{title}</title>
